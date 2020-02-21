aa3b776930165e369b1cf47efd99d28bb02dd64c
speak <speakk@gmail.com>
2020-02-21 21:10:55 +0200

    Menu hooray

--- states/mainMenu.lua
+++ states/mainMenu.lua
@@ -1,13 +1,18 @@
 local helium = require 'libs.helium'
 local input = require "libs.helium.core.input" 
 
+local headerFont = love.graphics.newFont("fonts/MavenPro-Medium.ttf", 128)
 local buttonFont = love.graphics.newFont("fonts/MavenPro-Medium.ttf", 48)
+
+local buttonW = 200
+local buttonH = 100
 
 local mainMenu = {}
 
 function mainMenu:initUI()
   local button = function(param,state,view)
-    local defaultColor = { 0.3, 0.5, 0.3 }
+    local defaultColor = { 0.3, 0.4, 0.5 }
+    local hoverColor = { 0.4, 0.6, 0.3 }
     local textColor = { 1,1,1,1 }
     local textShadowColor = { 0,0,0,0.5 }
 
@@ -20,7 +25,7 @@ function mainMenu:initUI()
     state.color = defaultColor
 
     input('hover', function ()
-      state.color = { 0.7, 0.2, 0.5 }
+      state.color = hoverColor
       return function ()
         state.color = defaultColor
       end
@@ -42,25 +47,53 @@ function mainMenu:initUI()
     end
   end
 
-  helium(button)({content="play"}, 200, 100):draw(400, 200)
+  local playButton = helium(button)({content="play"}, buttonW, buttonH)
+  playButton:draw(400, 200)
 
   -- local buttonFactory = helium(button)
   -- local button = buttonFactory({}, 200, 100)
   -- button:draw(400, 200)
 
-  -- return {
-  --   {
-  --     x = 400,
-  --     y = 100,
-  --     element = button
-  --   }
-  -- }
+  return {
+    playButton
+  }
 end
 
-function mainMenu:init()
-  self.initUI()
+local function recalcMenuPositions(ui, w, h)
+  local offsetY = 0
+  for i = 1,#ui do
+    local uiElement = ui[i]
+    uiElement.view.x = w/2 - buttonW/2
+    uiElement.view.y = h/2 - (#ui*buttonH)/2 + offsetY
+
+    offsetY = offsetY + buttonH
+  end
 end
+
+
+function mainMenu:init()
+  self.ui = self.initUI()
+  recalcMenuPositions(self.ui, love.graphics.getDimensions())
+end
+
+function mainMenu:resize(w, h)
+  recalcMenuPositions(self.ui, w, h)
+end
+
+function mainMenu:draw()
+  love.graphics.clear(0.08,0.02,0.1,1)
+  love.graphics.setFont(headerFont)
+  local w, h = love.graphics.getDimensions()
+  love.graphics.setColor(0.5,0.1,0.3,1)
+  love.graphics.printf("Lounge", 10, 100, w, 'center')
+  love.graphics.setColor(0.2,0.5,0.1,1)
+  love.graphics.printf("Lounge", 5, 100, w, 'center')
+  love.graphics.setColor(1,1,1,1)
+  love.graphics.printf("Lounge", 0, 100, w, 'center')
+end
+  
 -- 
+--
 -- function mainMenu:draw()
 --   for i = 1,#self.ui do
 --     local uiElement = self.ui[i]
